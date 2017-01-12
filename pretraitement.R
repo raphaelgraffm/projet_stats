3cadf2567df901340255229f9fdec3ebe85d4ae5
raphaelgraffm <raphgraff@live.fr>
2017-01-12 10:36:48 +0100

    Commit initial

---
+++ pretraitement.R
@@ -0,0 +1,55 @@
+donnees  <- read.csv("donnees.csv", sep=";", stringsAsFactors=FALSE)
+#summary(donnees)
+numcol <- ncol(donnees)
+numrow <- nrow(donnees)
+#print(numcol)
+#print(numrow)
+donnees1 <- donnees
+
+# Suppression des colonnes pour lesquelles on a peu d'informations
+# Et replissage par la moyenne des donnees manquantes
+invalid_cols = c()
+incomplete_cols = c()
+for (col in 1:numcol) {
+
+    num_valid_data <- 0
+    num_absent_data <- 0
+
+    for (row in 1:numrow) {
+        if (donnees[row,col] == "?") {
+            num_absent_data = num_absent_data + 1
+        }
+        else {
+            num_valid_data = num_valid_data + 1
+        }
+    }
+
+    #if (num_absent_data > num_valid_data) {
+    if (num_absent_data > 0) {
+        invalid_cols = c(invalid_cols, col)
+    }
+    #else if (num_absent_data > 0) {
+    #    incomplete_cols = c(incomplete_cols, col)
+    #}
+}
+
+#for (col in incomplete_cols) {
+#    somme <- 0
+#    for (row in 1:numrow) {
+#        if (donnees[row,col] != "?") {
+#            somme <- somme + as.numeric(as.character(donnees[row,col]))
+#        }
+#    }
+#    moyenne <- somme / numrow
+#    for (row in 1:numrow) {
+#        if (donnees[row,col] == "?") {
+#            donnees1[row,col] <- moyenne
+#        }
+#    }
+#}
+
+donnees1 <- donnees1[,-invalid_cols]
+save(donnees1, file = "donnees_traitees")
+
+
+#summary(donnees1)
---
+++ regression.R
@@ -0,0 +1,52 @@
+load("donnees_traitees")
+donnees = donnees1[!colnames(donnees1)=="communityname"]
+numcol <- ncol(donnees)
+numrow <- nrow(donnees)
+
+pourcentage_test <- 0.3
+
+seuil = floor( (1 - pourcentage_test) *numrow)
+
+donnees_entrainement <- donnees[1:seuil,]
+donnees_test <- donnees[seuil+1:numrow,]
+
+# Modele 1 : toutes les variables, regression purement linéaire
+modele1 <- "donnees_entrainement$ViolentCrimesPerPop~"
+for (nom in names(donnees)[1:(numcol-2)]) {
+    modele1 <- paste(modele1,"donnees_entrainement$",nom,"+",sep="")
+}
+modele1 <- paste(modele1,"donnees_entrainement$",names(donnees)[numcol-1],sep="")
+#modele1
+
+#result1 <- lm(modele1,data=donnees_entrainement)
+#summary(result1)
+
+# Modele 2 : uniquement les variables significatives du modèle précédent, toujours purement linéaire
+col2 <- c(1,5,10,14,17,18,21,26,28,30,35,40,41,50,63,66,68,69,70,71,73,76,77,80,81,84,87,90,92,100,numcol)
+donnees_entrainement_2 <- donnees_entrainement[col2]
+donnees_test_2 <- donnees_test[col2]
+numcol2 <- length(col2)
+
+modele2 <- "donnees_entrainement$ViolentCrimesPerPop~"
+for (nom in names(donnees_entrainement_2)[1:(numcol2-2)]) {
+    modele2 <- paste(modele2,"donnees_entrainement$",nom,"+",sep="")
+}
+modele2 <- paste(modele2,"donnees_entrainement$",names(donnees_entrainement_2)[numcol2-1],sep="")
+
+#result2 <- lm(modele2,data=donnees_entrainement_2)
+#summary(result2)
+
+# Modele 3 : nouveau tri des variables significatives
+col3 <- c(1,2,3,4,6,7,12,13,14,18,19,20,21,22,25,26,27,28,29,numcol2)
+donnees_entrainement_3 <- donnees_entrainement_2[col3]
+donnees_test_3 <- donnees_test_2[col3]
+numcol3 <- length(col3)
+
+modele3 <- "donnees_entrainement$ViolentCrimesPerPop~"
+for (nom in names(donnees_entrainement_3)[1:(numcol3-2)]) {
+    modele3 <- paste(modele3,"donnees_entrainement$",nom,"+",sep="")
+}
+modele3 <- paste(modele3,"donnees_entrainement$",names(donnees_entrainement_3)[numcol3-1],sep="")
+
+result3 <- lm(modele3,data=donnees_entrainement_3)
+summary(result3)
